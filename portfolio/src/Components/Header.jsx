4fa98a7b7a5aa5001ee521d9639dcf2a46dbf90f
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-21 22:34:51 +0530

    New:Header component.

---
+++ portfolio/src/Components/Header.jsx
@@ -0,0 +1,69 @@
+import { useState } from "react";
+import { Link } from "react-router-dom";
+
+const Header = () => {
+  const [isOpen, setIsOpen] = useState(false);
+
+  const toggleMenu = () => {
+    setIsOpen(!isOpen);
+  };
+
+  return (
+    <header className="bg-gray-800 text-white p-4 w-full font-serif">
+      <nav className="container mx-auto flex items-center justify-between">
+        <div className="text-xl font-bold">Maurice Minin Dsouza</div>
+        <div className="hidden md:flex space-x-4">
+          <Link to="/">Home</Link>
+          <Link to="/projects">Projects</Link>
+          <Link to="/skills">Skills</Link>
+          <Link to="/resume">Resume</Link>
+          <Link to="/contact">Contact</Link>
+        </div>
+        <div className="md:hidden">
+          <button onClick={toggleMenu} className="focus:outline-none">
+            <svg
+              className="w-6 h-6"
+              fill="none"
+              stroke="currentColor"
+              viewBox="0 0 24 24"
+              xmlns="http://www.w3.org/2000/svg"
+            >
+              <path
+                strokeLinecap="round"
+                strokeLinejoin="round"
+                strokeWidth="2"
+                d="M4 6h16M4 12h16m-7 6h7"
+              ></path>
+            </svg>
+          </button>
+        </div>
+      </nav>
+      {isOpen && (
+        <div className="md:hidden bg-gray-800">
+          <div className="flex flex-col space-y-2 p-4">
+            <Link to="/" onClick={toggleMenu}>
+              Home
+            </Link>
+            <Link to="/about" onClick={toggleMenu}>
+              About
+            </Link>
+            <Link to="/resume" onClick={toggleMenu}>
+              Resume
+            </Link>
+            <Link to="/projects" onClick={toggleMenu}>
+              Projects
+            </Link>
+            <Link to="/skills" onClick={toggleMenu}>
+              Skills
+            </Link>
+            <Link to="/contact" onClick={toggleMenu}>
+              Contact
+            </Link>
+          </div>
+        </div>
+      )}
+    </header>
+  );
+};
+
+export default Header;
