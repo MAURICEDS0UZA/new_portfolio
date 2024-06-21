5847eb52fd7e99b3adab4e9aa0c130c9a95ced61
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-21 22:26:02 +0530

    New:Skill component.

---
+++ portfolio/src/Components/Skill.jsx
@@ -0,0 +1,29 @@
+import { SKILLS } from "../utils/constant";
+
+const Skills = () => {
+  return (
+    <section className=" bg-gray-100 p-6 font-serif ">
+      <div className="max-w-4xl mx-auto">
+        <h2 className="text-3xl font-bold mb-8 text-center text-gray-900">
+          Skills
+        </h2>
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+          {SKILLS.map((skillCategory, index) => (
+            <div key={index} className="bg-white p-6 rounded-lg shadow-md">
+              <h3 className="text-xl font-semibold mb-4 text-gray-900">
+                {skillCategory.category}
+              </h3>
+              <ul className="  text-gray-700">
+                {skillCategory.skills.map((skill, idx) => (
+                  <li key={idx}>{skill}</li>
+                ))}
+              </ul>
+            </div>
+          ))}
+        </div>
+      </div>
+    </section>
+  );
+};
+
+export default Skills;
