4e8db8e613f3c8098dd23fa1a41c387c71f832a6
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-22 18:56:03 +0530

    Feat : Applied grid to Skill container.

--- portfolio/src/Components/Skill.jsx
+++ portfolio/src/Components/Skill.jsx
@@ -2,12 +2,12 @@ import { SKILLS } from "../utils/constant";
 
 const Skills = () => {
   return (
-    <section className=" bg-gray-100 p-6 font-serif ">
+    <section className="  p-6  py-20 bg-gray-100  font-serif ">
       <div className="max-w-4xl mx-auto">
         <h2 className="text-3xl font-bold mb-8 text-center text-gray-900">
           Skills
         </h2>
-        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8">
+        <div className="grid grid-cols-1 sm:grid-cols-2 lg:grid-cols-3 gap-8 ">
           {SKILLS.map((skillCategory, index) => (
             <div key={index} className="bg-white p-6 rounded-lg shadow-md">
               <h3 className="text-xl font-semibold mb-4 text-gray-900">
@@ -15,7 +15,9 @@ const Skills = () => {
               </h3>
               <ul className="  text-gray-700">
                 {skillCategory.skills.map((skill, idx) => (
-                  <li key={idx}>{skill}</li>
+                  <li key={idx} className="italic">
+                    {skill}
+                  </li>
                 ))}
               </ul>
             </div>
