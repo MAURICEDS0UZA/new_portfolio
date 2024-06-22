d6753df2be8041ff4729f99ffa94ef43aec5b50a
MAURICEDS0UZA <145907681+MAURICEDS0UZA@users.noreply.github.com>
2024-06-22 23:38:14 +0530

    Feat:Lazy loading.

--- portfolio/src/App.jsx
+++ portfolio/src/App.jsx
@@ -1,12 +1,15 @@
 import { BrowserRouter as Router, Route, Routes } from "react-router-dom";
 import Header from "./Components/Header";
-import Home from "./Components/Home";
-import Projects from "./Components/Projects";
 import Contact from "./Components/ContactUs";
 import Footer from "./Components/Footer";
 import Skills from "./Components/Skill";
-import Resume from "./Components/Resume";
 import resumePdf from "./assets/pdf/Maurice-Minin-Dsouza.pdf";
+
+import { lazy, Suspense } from "react";
+
+const Project = lazy(() => import("./Components/Projects"));
+const Resume = lazy(() => import("./Components/Resume"));
+const Home = lazy(() => import("./Components/Home"));
 
 function App() {
   return (
@@ -17,16 +20,32 @@ function App() {
           path="/"
           element={
             <>
-              <Home />
-              <Projects />
+              <Suspense fallback={<div>Loading ...</div>}>
+                <Home />
+                <Project />
+              </Suspense>
               <Skills />
             </>
           }
         />
-        <Route path="/projects" element={<Projects />} />
+        <Route
+          path="/projects"
+          element={
+            <Suspense fallback={<div>Loading Project...</div>}>
+              <Project />
+            </Suspense>
+          }
+        />
         <Route path="/skills" element={<Skills />} />
         <Route path="/contact" element={<Contact />} />
-        <Route path="/resume" element={resumePdf && <Resume />} />
+        <Route
+          path="/resume"
+          element={
+            <Suspense fallback={<div>Loading Resume...</div>}>
+              {resumePdf && <Resume />}
+            </Suspense>
+          }
+        />
       </Routes>
       <Footer />
     </Router>
